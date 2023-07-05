11c73221c4665157aae9e48aa13a2d9cc7723973
Matheus Rocha <monteiromatheus.rocha@gmail.com>
2023-07-05 00:35:26 -0300

    fix: show red spot correctly

--- app/mobile/components/SpotMarker.js
+++ app/mobile/components/SpotMarker.js
@@ -1,57 +1,70 @@
-import { StyleSheet, Text, View, Image, Alert, Platform} from 'react-native';
+import { StyleSheet, Text, View, Image, Alert, Platform } from 'react-native';
 import { Callout, Marker, CalloutSubview } from 'react-native-maps';
 import { Ionicons } from '@expo/vector-icons';
 import Paho from 'paho-mqtt';
 
-const SpotMarker = props => {
-  let iconName=""
-  let iconColor=""
-  
-  if (props.luminosity<1000) {
-    iconName="cloud"
-    iconColor="gray"
+const SpotMarker = (props) => {
+  let iconName = '';
+  let iconColor = '';
+
+  if (props.luminosity < 1000) {
+    iconName = 'cloud';
+    iconColor = 'gray';
   } else {
-    iconName="sunny"
-    iconColor="#FFD01C"
+    iconName = 'sunny';
+    iconColor = '#FFD01C';
   }
 
   function ReserveSpot(client, device, user, requested, status) {
-    if ((requested===null &&  status===false)) {
-      client.subscribe("devices/"+device+"/ask")
-      console.log("devices/"+device+"/ask")
+    if (requested === null && status === false) {
+      client.subscribe('devices/' + device + '/ask');
+      console.log('devices/' + device + '/ask');
       const message = new Paho.Message(user);
-      message.destinationName = "devices/"+device+"/reserve";
+      message.destinationName = 'devices/' + device + '/reserve';
       client.send(message);
-      console.log("trying to send:"+ message+" to: devices/"+device+"/reserve")
-    }
-    else if (status) {
-      Alert.alert("Vaga já ocupada", "Não foi possível reservar a essa vaga pois já está ocupada");
+      console.log(
+        'trying to send:' + message + ' to: devices/' + device + '/reserve'
+      );
+    } else if (status) {
+      Alert.alert(
+        'Vaga já ocupada',
+        'Não foi possível reservar a essa vaga pois já está ocupada'
+      );
     }
   }
-  
+
   return (
-    <Marker
-      coordinate={props.location}
-    >
+    <Marker coordinate={props.location}>
       <View style={{ width: 50, height: 50 }}>
         <Image
           source={
-            (props.status === true && props.situation === false)
+            props.status === true &&
+            (props.situation === false || props.requested === null)
               ? require('../assets/marker-occupied-bad.png')
-              : (props.status === false && props.requested === null)
+              : props.status === false && props.requested === null
               ? require('../assets/marker-free.png')
-              : (props.status === false && props.requested)
+              : props.status === false && props.requested
               ? require('../assets/marker-reserved.png')
-              : (props.situation === true && props.requested)
+              : props.situation === true && props.requested
               ? require('../assets/marker-occupied.png')
               : null
-          } 
+          }
           style={{ width: '100%', height: '100%' }}
           resizeMode="contain"
         />
       </View>
-      <Callout tooltip 
-      {...(Platform.OS === 'android' && { onPress: () => ReserveSpot(props.client, props.deviceName, props.user, props.requested, props.status) })}
+      <Callout
+        tooltip
+        {...(Platform.OS === 'android' && {
+          onPress: () =>
+            ReserveSpot(
+              props.client,
+              props.deviceName,
+              props.user,
+              props.requested,
+              props.status
+            ),
+        })}
       >
         <View style={styles.container}>
           <View style={styles.top}>
@@ -59,97 +72,141 @@ const SpotMarker = props => {
               <Text style={styles.titleText}>{props.name}</Text>
             </View>
             <View style={styles.icon}>
-              <Text style={{fontWeight: "bold", marginBottom: 5, fontSize: 18}}>{props.temperature}ºC</Text>
-              <Ionicons name={iconName} size={30} color={iconColor}/>
+              <Text
+                style={{ fontWeight: 'bold', marginBottom: 5, fontSize: 18 }}
+              >
+                {props.temperature}ºC
+              </Text>
+              <Ionicons name={iconName} size={30} color={iconColor} />
             </View>
           </View>
           {Platform.OS === 'ios' ? (
             <CalloutSubview
-            style={[
-              styles.status, 
-              (props.status === true && props.situation === false) ? styles.occupiedBad : null, 
-              (props.status === false && props.requested === null) ? styles.free : null, 
-              (props.status === false && props.requested) ? styles.reserved : null,
-              (props.status === true && props.situation) ? styles.occupied : null,  
-            ]} 
-            onPress={() => ReserveSpot(props.client, props.deviceName, props.user, props.requested, props.status)}
+              style={[
+                styles.status,
+                props.status === true && props.situation === false
+                  ? styles.occupiedBad
+                  : null,
+                props.status === false && props.requested === null
+                  ? styles.free
+                  : null,
+                props.status === false && props.requested
+                  ? styles.reserved
+                  : null,
+                props.status === true && props.situation
+                  ? styles.occupied
+                  : null,
+              ]}
+              onPress={() =>
+                ReserveSpot(
+                  props.client,
+                  props.deviceName,
+                  props.user,
+                  props.requested,
+                  props.status
+                )
+              }
             >
               <View>
-                {(props.status === true && props.situation === false) ? <Text style={styles.statusText}>Irregular</Text> : null} 
-                {(props.status === false && props.requested === null) ? <Text style={styles.statusText}>Livre</Text> : null}
-                {(props.status === false && props.requested) ? <Text style={styles.statusText}>Reservada</Text> : null}
-                {(props.status === true && props.situation) ? <Text style={styles.statusText}>Ocupada</Text> : null }
+                {props.status === true && props.situation === false ? (
+                  <Text style={styles.statusText}>Irregular</Text>
+                ) : null}
+                {props.status === false && props.requested === null ? (
+                  <Text style={styles.statusText}>Livre</Text>
+                ) : null}
+                {props.status === false && props.requested ? (
+                  <Text style={styles.statusText}>Reservada</Text>
+                ) : null}
+                {props.status === true && props.situation ? (
+                  <Text style={styles.statusText}>Ocupada</Text>
+                ) : null}
               </View>
             </CalloutSubview>
           ) : (
             <View
               style={[
-                styles.status, 
-                (props.status === true && props.situation === false) ? styles.occupiedBad : null, 
-                (props.status === false && props.requested === null) ? styles.free : null, 
-                (props.status === false && props.requested) ? styles.reserved : null,
-                (props.status === true && props.situation) ? styles.occupied : null,  
+                styles.status,
+                props.status === true && props.situation === false
+                  ? styles.occupiedBad
+                  : null,
+                props.status === false && props.requested === null
+                  ? styles.free
+                  : null,
+                props.status === false && props.requested
+                  ? styles.reserved
+                  : null,
+                props.status === true && props.situation
+                  ? styles.occupied
+                  : null,
               ]}
-              >
-              {(props.status === true && props.situation === false) ? <Text style={styles.statusText}>Irregular</Text> : null} 
-              {(props.status === false && props.requested === null) ? <Text style={styles.statusText}>Livre</Text> : null}
-              {(props.status === false && props.requested) ? <Text style={styles.statusText}>Reservada</Text> : null}
-              {(props.status === true && props.situation) ? <Text style={styles.statusText}>Ocupada</Text> : null }
+            >
+              {props.status === true && props.situation === false ? (
+                <Text style={styles.statusText}>Irregular</Text>
+              ) : null}
+              {props.status === false && props.requested === null ? (
+                <Text style={styles.statusText}>Livre</Text>
+              ) : null}
+              {props.status === false && props.requested ? (
+                <Text style={styles.statusText}>Reservada</Text>
+              ) : null}
+              {props.status === true && props.situation ? (
+                <Text style={styles.statusText}>Ocupada</Text>
+              ) : null}
             </View>
           )}
         </View>
       </Callout>
     </Marker>
-    );
+  );
 };
 
-export default SpotMarker
+export default SpotMarker;
 
 const styles = StyleSheet.create({
-    container: {
-      width: 200,
-      backgroundColor: "white",
-      borderRadius: 15,
-      padding: 10
-    },
-    top: {
-      flexDirection: "row"
-    },
-    title: {
-      flex: 2,
-    },
-    titleText: {
-      fontWeight: "bold",
-      fontSize: 20
-    },
-    icon: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center"
-    },
-    status: {
-      flex: 2,
-      margin: 3,
-      borderRadius: 15,
-      justifyContent: "center",
-      alignItems:"center"
-    },
-    statusText: {
-      color: "white",
-      fontWeight: "bold",
-      fontSize: 28,
-      padding: 5
-    },
-    free: {
-      backgroundColor: "green"
-    },
-    reserved: {
-      backgroundColor: "#4691ef"
-    },
-    occupied: {
-      backgroundColor: "#9222F2"
-    },
-    occupiedBad: {
-      backgroundColor: "red"
-    }
-  });
+  container: {
+    width: 200,
+    backgroundColor: 'white',
+    borderRadius: 15,
+    padding: 10,
+  },
+  top: {
+    flexDirection: 'row',
+  },
+  title: {
+    flex: 2,
+  },
+  titleText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  icon: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  status: {
+    flex: 2,
+    margin: 3,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  statusText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 28,
+    padding: 5,
+  },
+  free: {
+    backgroundColor: 'green',
+  },
+  reserved: {
+    backgroundColor: '#4691ef',
+  },
+  occupied: {
+    backgroundColor: '#9222F2',
+  },
+  occupiedBad: {
+    backgroundColor: 'red',
+  },
+});
